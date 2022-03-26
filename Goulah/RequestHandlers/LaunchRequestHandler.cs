84bc4188bd6939187f8ef177293575fc308ef5cc
Roger Leblanc <rleblanc@mmbs.ca>
2022-03-26 11:53:54 -0400

    Fix welcome message

--- Goulah/RequestHandlers/LaunchRequestHandler.cs
+++ Goulah/RequestHandlers/LaunchRequestHandler.cs
@@ -1,25 +1,16 @@
 ﻿using Alexa.NET;
 using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
+using GoulahAlexaSkill.ResponseTexts;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace GoulahAlexaSkill.RequestHandlers
 {
-    public class LaunchRequestHandler : BaseRequestHandler<LaunchRequest>
+    public class LaunchRequestHandler : BaseRequestHandler<LaunchRequest, LaunchResponses>
     {
         public LaunchRequestHandler(LaunchRequest request) : base(request)
         {
-        }
-
-        public override async Task<IActionResult> GetResultAsync()
-        {
-            await Task.CompletedTask;
-
-            string speechText = "Bienvenue dans la skill Alexa de Codeurs Trois-Rivières, que puis-je faire pour vous?";
-
-            SkillResponse response = ResponseBuilder.Ask(speechText, RequestHandlerHelper.GetDefaultReprompt());
-            return new OkObjectResult(response);
         }
     }
 }
---
+++ Goulah/ResponseTexts/LaunchResponses.cs
@@ -0,0 +1,12 @@
+﻿namespace GoulahAlexaSkill.ResponseTexts
+{
+    public class LaunchResponses : BaseResponses
+    {
+        public LaunchResponses()
+        {
+            Add("Salut, c'est Goulah, qu'est-ce que tu veux?");
+            Add("Ouin, fais ça vite.");
+            Add("J'ai pas le temps, mais pose ta question quand même.");
+        }
+    }
+}
