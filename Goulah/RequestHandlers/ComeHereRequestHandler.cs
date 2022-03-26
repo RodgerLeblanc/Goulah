68839c07ca487ce66924409b0abef09e72614e5a
Roger Leblanc <rleblanc@mmbs.ca>
2022-03-26 12:35:53 -0400

    Add come here intent

---
+++ Goulah/RequestHandlers/ComeHereRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using Alexa.NET.Request.Type;
+using GoulahAlexaSkill.ResponseTexts;
+
+namespace GoulahAlexaSkill.RequestHandlers
+{
+    public class ComeHereRequestHandler : BaseRequestHandler<IntentRequest, ComeHereResponses>
+    {
+        public ComeHereRequestHandler(IntentRequest request) : base(request)
+        {
+        }
+    }
+}
---
+++ Goulah/ResponseTexts/ComeHereResponses.cs
@@ -0,0 +1,12 @@
+﻿namespace GoulahAlexaSkill.ResponseTexts
+{
+    public class ComeHereResponses : BaseResponses
+    {
+        public ComeHereResponses()
+        {
+            Add("Pardon? Toi, viens icitte.");
+            Add("Moi, venir icitte? C'est qui qui parle? T'es qui toé?");
+            Add("Si je dois me déplacer, yé mieux d'avoir de la bière.");
+        }
+    }
+}
